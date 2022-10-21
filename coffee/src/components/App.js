64655700e013de8197089f2344d4cc02dfde748f
Ryan Broadsword <rbroadsword@gmail.com>
2022-10-20 22:43:57 -0700

    add app.js

---
+++ coffee/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import Header from "./Header"; 
+import CoffeeControl from "./CoffeeControl";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <CoffeeControl />
+    </React.Fragment>
+  ); 
+}
+
+export default App;
