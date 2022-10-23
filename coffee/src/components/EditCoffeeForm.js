9181627b08ee878ae346597fd011d89c960ef7b8
Ryan Broadsword <rbroadsword@gmail.com>
2022-10-23 10:25:14 -0700

    edit coffee form

---
+++ coffee/src/components/EditCoffeeForm.js
@@ -0,0 +1,28 @@
+import React from "react"; 
+import ReusableForm from "./ReusableForm"; 
+import PropTypes from "prop-types"; 
+
+function EditCoffeeForm(props) {
+
+  const { coffee } = props; 
+
+  function handleEditCoffeeFormSubmission(event) {
+    event.preventDefault(); 
+    props.onEditCoffee({names: event.target.name.value, quantity: event.target.quantity.value, description: event.target.description.value, id: coffee.id});
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditCoffeeFormSubmission}
+        buttonText="Update Coffee" />
+    </React.Fragment>
+  ); 
+}
+
+EditCoffeeForm.propTypes = {
+  coffee: PropTypes.object, 
+  onEditCoffee: PropTypes.func
+}; 
+
+export default EditCoffeeForm; 
